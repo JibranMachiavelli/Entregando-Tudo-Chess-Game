b338139cbcf01b26dd3392679b146d124bbdedb5
JibranMachiavelli <jibranind31@gmail.com>
2024-05-16 20:53:43 -0300

    feat: pageInitial semi-concluida

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,18 +7,29 @@ import Text from './components/general/Text';
 export default function initialPage() {
   return (
     <>
-      <div className="bg-red-600 text-2xl">
-        <Text size="lg">
-          blabla
-          <Link href="../../home">Home</Link>
-        </Text>
-      </div>
-      <div className="bg-red-600 text-2xl">
-        <Link href="../../game">Game</Link>
-      </div>
-      <div className="bg-red-600 text-2xl">
-        <Link href="../../login">login</Link>
-      </div>
+      <Link href="./home" className="cursor-default">
+        <div className="h-screen">
+          <div className="flex items-center ">
+            <div className="border-b-2 m-4 w-full"></div>
+            <div className="w-12 h-12 m-4 relative">
+              <Image
+                src=""
+                alt="Icon Player"
+                layout="fill"
+                objectFit="cover"
+                className="rounded-full bg-red-500"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-center ">
+            <div className="text-center m-64">
+              <h1 className="text-8xl">Chess</h1>
+              <h2 className="text-6xl">Legends</h2>
+            </div>
+            <p>Tap for continue...</p>
+          </div>
+        </div>
+      </Link>
     </>
   );
 }
